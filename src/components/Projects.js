86ce4d7f5c0a9ece987e57b4aa6dbff13eb61396
farrahpepino <idcfarrah@gmail.com>
2025-09-13 14:09:35 -0400

    updated project.cs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,42 +6,39 @@ const Projects = () => {
         <div id="carouselExampleCaptions" class="carousel slide">
             <div class="carousel-inner">  
                 <div class="carousel-item active">
-<<<<<<< HEAD
-=======
         
->>>>>>> 84a3fed0533ea5035d360339e07a752f387961cc
                 <img src="/assets/loop.png" class="d-block w-100" alt="..." />
                     <div className='b-mt'>
                     <button className='b-main b-main-hover b'><a href="https://www.youtube.com/watch?v=U8A_GpiDMRo" target="_blank">DEMO</a></button>
-                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/SocialNetworkingApp' target="_blank">CODE</a></button>
+                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/social-networking-app' target="_blank">CODE</a></button>
                     </div>
                 </div>
                 <div class="carousel-item">
                 <img src="/assets/aia.png" class="d-block w-100" alt="..." />
                     <div className='b-mt'>
                     <button className='b-main b-main-hover b'><a href='https://www.youtube.com/watch?v=AoDgb6FLF0o' target="_blank">DEMO</a></button>
-                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/EcommerceApp' target="_blank">CODE</a></button>
+                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/ecommerce-app' target="_blank">CODE</a></button>
                     </div>
                 </div>
                 <div class="carousel-item">
                 <img src="/assets/amore.png" class="d-block w-100" alt="..." />
                     <div className='b-mt'>
                     <button className='b-main b-main-hover b'><a href='https://farrahpepino-poemgenerator.netlify.app/' target="_blank">DEMO</a></button>
-                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/PoemGenerator' target="_blank">CODE</a></button>
+                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/poem-generator' target="_blank">CODE</a></button>
                     </div>
                 </div>
                 <div class="carousel-item">
                 <img src="/assets/weather.png" class="d-block w-100" alt="..." />
                     <div className='b-mt'>
                     <button className='b-main b-main-hover b'><a href='https://farrahpepino-weatherapp.netlify.app/' target="_blank">DEMO</a></button>
-                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/WeatherApp' target="_blank">CODE</a></button>
+                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/weather-app' target="_blank">CODE</a></button>
                     </div>
                 </div>
                 <div class="carousel-item">
                 <img src="/assets/tala-mobile.png" class="d-block w-100" alt="..." />
                     <div className='b-mt'>
                     <button className='b-main b-main-hover b'><a href='https://www.figma.com/design/ayF3aZh1dVA11O7Edkl9Bh/Archives-FINAL?node-id=0-1&t=JbxM6CBUdYG0jCQc-1' target="_blank">DESIGN</a></button>
-                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/PhotosharingApp-Mobile' target="_blank">CODE</a></button>
+                    <button className='b-main b-main-hover b'><a href='https://github.com/farrahpepino/photosharing-app-mobile' target="_blank">CODE</a></button>
                     </div>
                 </div>
             </div>
